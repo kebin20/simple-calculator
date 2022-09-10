0a6106f2b586c7e90818569cdf44c2f7763c6d5c
kebin20 <ktanzyl@hotmail.com>
2022-09-10 12:06:22 +0900

    Added functions, updated Readme to reflect changes

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # simple-calculator
 Simple calculator app coded with HTML, CSS and Vanilla Javascript
 
+Things of note in my Javascript code:
+
+- I used forEach to target all of the number and operator buttons
+
+-  In order to obtain the value from the buttons, I then specified the event being targeted which in this case is the textContent of the HTML element 
+
+- I was struggling in how NOT to double up the inputted number onto the display screen e.g when 6 and the + button is pressed, the display screen would instantly show  6 + 6 before the second number was even pressed. 
+
+Therefore, in order to fix this, in the operatorHandler function, valueOne will need to equal to value Two first, then valueTwo must be made as an empty string.
+
+- In order to have the numbers properly separated by commas, I used parseInt() first to convert the valueOne string to a number, and then used toLocaleString();
+
+
+
+THINGS TO NOTE: 
+
+- Fiddle with the valueOne and valueTwo lengths to show correct separators and decimal separators
+
+
+Attempts:
+
+- I tried to attempt to use a switch statement for the calculation, but it didn't calculate properly.
+
+
+Functions that I want to include: 
+
+- Reset the calculator by pressing any number buttons once the calculation has been made 
+
+- Giving the function of being able to further calculate from the result after adding an operator to the caclulated result.
--- app.js
+++ app.js
@@ -1,26 +1,105 @@
+//Global variables
+let valueOne = "";
+let valueTwo = "";
+let operatorValue = "";
+
 // HTML Components
 document.addEventListener("DOMContentLoaded", () => {
-    let clear = document.querySelector(".clear");
-    let equal = document.querySelector(".equal");
-    let decimal = document.querySelector(".decimal");
-    
-    let numbers = document.querySelectorAll(".number");
-    let operators = document.querySelectorAll(".operator");
+  let clear = document.querySelector(".clear");
+  let equal = document.querySelector(".equal");
+  let decimal = document.querySelector(".decimal");
+  let result = document.querySelector(".result");
 
-    let oldValue = ""
-    let newValue= ""
-    
-   operators.forEach(operator => operator.addEventListener('click', (e) => {
-      userInput(e.target.textContent);
-    }))
-    
-    numbers.forEach(number => number.addEventListener('click', (e) => {
-      userInput(e.target.textContent);
-    }))
-  })
-  
-  function userInput(num) {
-    console.log(num)
+  let numbers = document.querySelectorAll(".number");
+  let operators = document.querySelectorAll(".operator");
+
+  let prevScreen = document.querySelector("#prev-screen");
+  let nextScreen = document.querySelector("#next-screen");
+
+  numbers.forEach((number) =>
+    number.addEventListener("click", (e) => {
+      //NOTE: In order to get the value from the HTML element, you need to specify the event being targeted (e.target), which in this case is the textContent
+      numberHandler(e.target.textContent);
+      if (valueTwo.length <= 11) {
+        nextScreen.textContent = valueTwo;
+      } else {
+        result.textContent = parseInt(valueTwo).toLocaleString();
+      }
+    })
+  );
+
+  operators.forEach((operator) =>
+    operator.addEventListener("click", (e) => {
+      operatorHandler(e.target.textContent);
+      prevScreen.textContent = `${valueOne} ${operatorValue} `;
+      nextScreen.textContent = valueTwo;
+    })
+  );
+
+  clear.addEventListener("click", () => {
+    valueOne = "";
+    valueTwo = "";
+    operatorValue = "";
+    nextScreen.textContent = valueOne;
+    prevScreen.textContent = valueTwo;
+    result.textContent = "";
+  });
+
+  equal.addEventListener("click", () => {
+    //NOTE: This parent if statement is for when the user clicks on the equal button, it will not display anything
+    if (valueOne != "" && valueTwo != "") {
+      calculate();
+      if (valueOne.length <= 6) {
+        result.textContent = valueOne;
+      } else {
+        result.textContent = parseInt(valueOne).toLocaleString();
+      }
+    }
+  });
+
+  decimal.addEventListener("click", () => {
+    addDecimal();
+  });
+});
+
+function numberHandler(num) {
+  if (valueTwo.length <= 10) {
+    valueTwo += num;
   }
-  
-                        
+}
+
+function operatorHandler(operator) {
+  operatorValue = operator;
+  valueOne = valueTwo;
+  //NOTE: In order to NOT double up the number that is being inputted onto the display, valueTwo variable  should be made empty
+  valueTwo = "";
+}
+
+function calculate() {
+  valueOne = Number(valueOne);
+  valueTwo = Number(valueTwo);
+
+  if (operatorValue === "+") {
+    valueOne += valueTwo;
+  } else if (operatorValue === "-") {
+    valueOne -= valueTwo;
+  } else if (operatorValue === "÷") {
+    valueOne /= valueTwo;
+  } else if (operatorValue === "X") {
+    valueOne *= valueTwo;
+  }
+
+  valueOne = roundNum(valueOne);
+  valueOne = valueOne.toString();
+  valueTwo = valueTwo.toString();
+}
+
+function roundNum(num) {
+  return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal() {
+  if (!valueTwo.includes(".")) {
+    valueTwo += ".";
+  }
+}
